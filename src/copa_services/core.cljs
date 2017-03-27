8ae05c5f476cf0a24db26707494c85f8bcb428f0
dviramontes <dviramontes@gmail.com>
2017-03-27 01:01:36 -0600

    feat: parse from and body from twilio sms payload

--- src/copa_services/core.cljs
+++ src/copa_services/core.cljs
@@ -4,11 +4,28 @@
 
 (def twilio (nodejs/require "twilio"))
 
+(def request (nodejs/require "request"))
+
+(def query-string (nodejs/require "querystring"))
+
 (def twiml (twilio.TwimlResponse.))
 
-(defgateway echo [event ctx]
-  {:status  200
-   :headers {:content-type "text/xml"}
-   :body    (-> twiml
-                (.message "hola camarada!")
-                (.toString))})
+(defonce copa-firebase-endpoint
+         "https://copa-services-storage.firebaseio.com/members/onboard.json")
+
+(defonce request-opts {:uri    copa-firebase-endpoint
+                       :method "PUT"
+                       :json   {:foo "bar"}})
+
+(defgateway echo [{:keys [body] :as input} ctx]
+            (let [parsed-query-str (query-string.parse body)
+                  sms-body (aget parsed-query-str "Body")
+                  sms-from (aget parsed-query-str "From")]
+              (js/console.log (str sms-from ":" sms-body))
+              (request (clj->js request-opts)
+                       (fn [err res res-body]
+                         {:status  200
+                          :headers {:content-type "text/xml"}
+                          :body    (-> twiml
+                                       (.message (str sms-from ":" sms-body))
+                                       (.toString))}))))
