a24601692e896b1ff8126252a29cfd52ad873205
dviramontes <dviramontes@gmail.com>
2017-04-12 19:07:53 -0600

    wip: branch logic

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject copa-services "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure       "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
-                 [io.nervous/cljs-lambda    "0.3.5"]]
+                 [io.nervous/cljs-lambda    "0.3.5"]
+                 [org.clojure/core.async    "0.2.395"]
+                 [jamesmacaulay/cljs-promises "0.1.0"]]
   :plugins [[lein-npm                    "0.6.2"]
             [io.nervous/lein-cljs-lambda "0.6.5"]]
   :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]
--- src/copa_services/core.cljs
+++ src/copa_services/core.cljs
@@ -1,74 +1,79 @@
 (ns copa-services.core
   (:require [cljs-lambda.macros :refer-macros [defgateway]]
-            [cljs.nodejs :as nodejs]))
+            [cljs.core.async :as async]
+            [cljs.nodejs :as nodejs])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def query-string (nodejs/require "querystring"))
 
 (def http (nodejs/require "request-promise"))
 
 (defonce copa-firebase-endpoint
-  "https://copa-services-storage.firebaseio.com")
+         "https://copa-services-storage.firebaseio.com")
 
 (defonce outgoing-messages
-         {:step-0 "Thank you for signing up to receive COPA’s text messages! To provide you the correct information, please provide your preferred language. Reply “1” for English or “2” for Spanish. \n\n¡Gracias por inscribirse a los mensajes de COPA! Para darle la información correcta, por favor designe su idioma preferido. Responda “1” para inglés o “2” para español."
-          :step-1 {:es-US "¡Gracias! Su idioma preferido es español. Y para comunicarnos mejor, responda con su nombre completo."
-                   :en-US "Thank you! Your preferred language is English. To best communicate with you, please respond with your full name."}})
+         {:step-0 "Welcome to COPA's text alerts and news! To subscribe to receive important updates reply \"yes.\" Do not respond if you do not want to be added at this time.\n\nBienvenid@ a los alertos de texto de COPA! Para inscribirse a recibir información importante, responda \"yes.\" No necesita responder si no quiere recibir mensajes por el momento."
+          :step-1 "Thank you for signing up to receive COPA’s text messages! To provide you the correct information, please provide your preferred language. Reply “1” for English or “2” for Spanish. \n\n¡Gracias por inscribirse a los mensajes de COPA! Para darle la información correcta, por favor designe su idioma preferido. Responda “1” para inglés o “2” para español."
+          :step-2 {:es-US "¡Gracias! Su idioma preferido es español. Y para comunicarnos mejor, responda con su nombre completo."
+                   :en-US "Thank you! Your preferred language is English. To best communicate with you, please respond with your full name."}
+          :step-3 {:es-US "¡Gracias! ¡Este pendiente de la información de COPA!"
+                   :en-US "Thank you! Stay tuned for updates from COPA!"}})
+
+(defn make-sms [twiml msg]
+  {:status  200
+   :headers {:content-type "text/xml"}
+   :body    (-> twiml
+                (.message msg)
+                (.toString))})
+
+(defn get-user [url]
+  (http (clj->js {:url url :method "GET" :json true})))
+
+
+(defn update-user-record
+  "creates or updates user record in firebase"
+  [url body]
+  (http (clj->js {:url url :method "PUT" :json true :body body})))
+
+
+;; TODO: upcase ppl's name, grown up case
+;; no yelling!
 
 (defgateway echo [{:keys [body] :as input} ctx]
             (let [twilio (nodejs/require "twilio")
                   twiml (twilio.TwimlResponse.)
                   parsed-query-str (query-string.parse body)
+                  sms-date (aget parsed-query-str "DateSent")
                   sms-body (aget parsed-query-str "Body")
-                  sms-from (aget parsed-query-str "From")
+                  sms-from (re-find #"\d+" (aget parsed-query-str "From"))
                   url (str copa-firebase-endpoint "/incoming/" sms-from ".json")
-                  get-request {:url    url
-                               :method "GET"
-                               :json   true}
-                  put-request {:url    url
-                               :method "PUT"
-                               :json   true}]
+                  body (clojure.string/upper-case sms-body)]
 
-              ;; step-0
-              ;; set receive preference
+              (js/console.log sms-from)
+              (js/console.log body)
 
-              (-> (http (clj->js get-request))
-                  (.then
-                    (fn [user]
-                      (js/console.log user)
+              (-> (get-user url)
+                  (.then (fn [user]
+                          (js/console.log user)
+                          (if (nil? (js->clj user))
+                            ;; firabase has no record
+                            (if (= body "YES")
+                              (-> (update-user-record url {:created sms-date})
+                                  (.then #(make-sms twiml (:step-1 outgoing-messages))))
+                              (make-sms twiml (:step-0 outgoing-messages)))
+                            ;; firebase has record
+                            (let [_user (clj->js user)]
 
-                      ;; if no data then first time user
-                      ;; create user record in firebase db
-
-                      (if-not user
-
-                        (do
-
-                          ;; put user at step-0
-                          ;; write to firebase
-
-                          (-> (http (clj->js (assoc put-request
-                                               :body {:step-0 (clojure.string/upper-case sms-body)})))
-
-                              (.then (fn [_]
-                                       ;; reply via sms
-                                       {:status  200
-                                        :headers {:content-type "text/xml"}
-                                        :body    (-> twiml
-                                                     (.message (:step-0 outgoing-messages))
-                                                     (.toString))}))))
-
-                        ;; step-1
-                        ;; set lang preference
-
-                        (do
-                          (let [step-0-answer (aget user "step-0")]
-                            (if (= step-0-answer "YES")
-                              (do
-                                (-> (http (clj->js (assoc put-request
-                                                     :body {:step-1 (clojure.string/upper-case sms-body)})))
-                                    (.then (fn [_]
-                                             {:status  200
-                                              :headers {:content-type "text/xml"}
-                                              :body    (-> twiml
-                                                           (.message (-> outgoing-messages :step-1 :en-US))
-                                                           (.toString))})))))))))))))
+                              (if-let [lang (:lang _user)]
+                                ;; store name
+                                (-> (update-user-record url (assoc _user :name body))
+                                    (.then #(make-sms twiml (get-in outgoing-messages [:step-3 (keyword lang)]))))
+                                ;; store lang
+                                (let [lang (cond
+                                            (= body "1") "en-US"
+                                            (= body "2") "es-US"
+                                            :else "bail")]
+                                  (if-not (= lang "bail")
+                                    (-> (update-user-record url (assoc _user :lang lang))
+                                        (.then #(make-sms twiml (get-in outgoing-messages [:step-2 (keyword lang)]))))
+                                    (make-sms twiml (:step-1 outgoing-messages))))))))))))
