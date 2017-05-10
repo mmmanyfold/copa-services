49879c2c7481e41169a65afa92cb418c10d24c2e
David A. Viramontes <dviramontes@gmail.com>
2017-05-10 00:05:00 -0600

    feat: 18 (#19)
    * feat: email even if no new members
    
    * feat: adds START/STOP functionality
    
    * feat: rm special chars with regex

--- src/copa_services/core.cljs
+++ src/copa_services/core.cljs
@@ -46,12 +46,6 @@
   (let [opts {:url url :method "PUT" :json true}]
     (http (clj->js (assoc opts :body body)))))
 
-;; define a constant set of user properties
-(defonce USER-PROPS #{:created :name :lang :archived})
-
-;; TODO: upcase ppl's name, grown up case
-;; no yelling!
-
 (defgateway sms [{:keys [body] :as input} ctx]
             (let [twilio (nodejs/require "twilio")
                   twiml (twilio.TwimlResponse.)
@@ -59,43 +53,66 @@
                   sms-body (aget parsed-query-str "Body")
                   sms-from (re-find #"\d+" (aget parsed-query-str "From"))
                   url (str copa-firebase-endpoint "/incoming/" sms-from ".json")
-                  body (clojure.string/upper-case sms-body)]
+                  body (clojure.string/upper-case sms-body)
+                  body (-> body
+                           (.trim)
+                           (.replace (js/RegExp. "[^a-zA-Z0-9 ]" "g") ""))]
 
               (-> (GET url)
                   (.then (fn [user]
                            (if (nil? user)
                              ;; firabase has no record
                              (if (= body "YES")
-                               (-> (PUT url {:archived false
-                                             :created  (.now js/Date)})
+                               (-> (PUT url {:status "new"})
                                    (.then #(make-sms twiml (:step-1 outgoing-messages))))
                                (make-sms twiml (:step-0 outgoing-messages)))
                              ;; firebase has record
                              (let [user-map (js->clj user :keywordize-keys true)
                                    user-props ((comp set keys) user-map)]
-                               (when-not (= user-props USER-PROPS)
-                                 (if-let [lang (:lang user-map)]
-                                   ;; store name
-                                   (-> (PUT url (assoc user-map :name body))
-                                       (.then #(make-sms twiml (get-in outgoing-messages [:step-3 (keyword lang)]))))
-                                   ;; store lang
-                                   (let [lang (cond
-                                                (= body "1") "english"
-                                                (= body "2") "spanish"
-                                                :else "bail")]
-                                     (if-not (= lang "bail")
-                                       (-> (PUT url (assoc user-map :lang lang))
-                                           (.then #(make-sms twiml (get-in outgoing-messages [:step-2 (keyword lang)]))))
-                                       (make-sms twiml (:step-1 outgoing-messages)))))))))))))
 
-(defn update-archived-flag [user-records filtered]
+                               (cond
+                                 (= body "STOP")
+                                 (-> (PUT url (assoc user-map :status "REMOVE"))
+                                     (.then #(println "User removed.")))
+
+                                 (= body "START")
+                                 (let [user-map (assoc user-map :status "new")]
+                                   (if (not= user-props #{:name :lang :status})
+                                     (if-let [lang (:lang user-map)]
+                                       ;; ask name
+                                       (-> (PUT url user-map)
+                                           (.then #(make-sms twiml (get-in outgoing-messages [:step-2 (keyword lang)]))))
+                                       ;; ask lang
+                                       (-> (PUT url user-map)
+                                           (.then #(make-sms twiml (:step-1 outgoing-messages)))))
+                                     (-> (PUT url user-map)
+                                         (.then #(println "User opted back in.")))))
+
+                                 :else
+                                 (when (and (not= (:status user-map) "REMOVE")
+                                            (not= user-props #{:name :lang :status}))
+                                   (if-let [lang (:lang user-map)]
+                                     ;; store name
+                                     (-> (PUT url (assoc user-map :name body))
+                                         (.then #(make-sms twiml (get-in outgoing-messages [:step-3 (keyword lang)]))))
+                                     ;; store lang
+                                     (let [lang (cond
+                                                  (= body "1") "english"
+                                                  (= body "2") "spanish"
+                                                  :else :bail)]
+                                       (if-not (= lang :bail)
+                                         (-> (PUT url (assoc user-map :lang lang))
+                                             (.then #(make-sms twiml (get-in outgoing-messages [:step-2 (keyword lang)]))))
+                                         (make-sms twiml (:step-1 outgoing-messages))))))))))))))
+
+(defn update-status [user-records filtered]
   (let [records-to-update (map :number filtered)
         url (str copa-firebase-endpoint "/incoming.json")
         clj->users (js->clj user-records :keywordize-keys true)
         final-records-update
         (->> (map (fn [[k v]]
                     (if (some #(= k %) records-to-update)
-                      (hash-map k (update v :archived (constantly true)))
+                      (hash-map k (update v :status (constantly "exported")))
                       (hash-map k v))) clj->users)
              (into {}))]
     (-> (PUT url final-records-update)
@@ -103,7 +120,7 @@
 
 
 (defgateway email [{:keys [body] :as input} ctx]
-            (let [fields [:created :name :lang :number]
+            (let [fields [:status :name :lang :number]
                   url (str copa-firebase-endpoint "/incoming.json")
                   mailgun (mailgun-js (clj->js {:apiKey MAILGUN_KEY
                                                 :domain DOMAIN}))]
@@ -115,13 +132,13 @@
                             k (keys users)
                             v (vals users)
                             flat-users (map #(assoc %1 :number %2) v k)
-                            filter-archived (filter #(not (% :archived)) flat-users)]
-                        (let [csv (json2csv (clj->js {:data   filter-archived
+                            filter-for-export (filter #(not= (% :status) "exported") flat-users)]
+                        (let [csv (json2csv (clj->js {:data   filter-for-export
                                                       :fields fields}))
                               attch (.-Attachment mailgun)
                               attachment (attch. (clj->js {:data     (js/Buffer. csv)
                                                            :filename "members.csv"}))
-                              new-member-count (count filter-archived)
+                              new-member-count (count filter-for-export)
                               data {:from       EMAIL_FROM
                                     :to         EMAIL_TO
                                     :subject    (str "Hello, " new-member-count " new members today.")
@@ -133,5 +150,8 @@
                                 (.send (clj->js data)
                                        (fn [err body]
                                          (when-not err
-                                           (update-archived-flag json-user-records filter-archived)))))
-                            (println "No new members today.")))))))))
+                                           (update-status json-user-records filter-for-export)))))
+                            (-> mailgun
+                                (.messages)
+                                (.send (clj->js (dissoc data :attachment))
+                                       #(println "No new members today.")))))))))))
