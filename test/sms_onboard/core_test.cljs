10bb19b601b60b95b11f20dbb63072324b563b8e
David Viramontes <dviramontes@gmail.com>
2017-07-29 13:34:19 -0600

    test: get-env fn

--- test/sms_onboard/core_test.cljs
+++ test/sms_onboard/core_test.cljs
@@ -1,51 +1,16 @@
 (ns sms-onboard.core-test
-  (:require [sms-onboard.core :refer [work-magic config]]
-            [cljs.test :refer-macros [deftest is]]
-            [cljs-lambda.local :refer [invoke channel]]
-            [promesa.core :as p :refer [await] :refer-macros [alet]]
-            [cljs.core.async :refer [<!]]))
-  ;(:require-macros [cljs.core.async.macros :refer [go]]))
+  (:require [sms-onboard.helpers :refer [get-env]]
+            [cljs.test :refer-macros [deftest is testing use-fixtures]]
+            [cljs.nodejs :as nodejs]
+            [cljs-lambda.local :refer [invoke channel]]))
 
-;(defn with-promised-completion [p]
-;  (cljs.test/async
-;   done
-;   (-> p
-;       (p/catch #(is (not %)))
-;       (p/then done))))
-;
-;(defn with-some-error [p]
-;  (p/branch p
-;    #(is false "Expected error")
-;    (constantly nil)))
-;
-;(deftest echo
-;  (-> (invoke work-magic {:magic-word "not the magic word"})
-;      with-some-error
-;      with-promised-completion))
-;
-;(def delay-channel-req
-;  {:magic-word (:magic-word config)
-;   :spell :delay-channel
-;   :msecs 2})
-;
-;(deftest delay-channel-spell
-;  (with-promised-completion
-;    (alet [{:keys [waited]} (await (invoke work-magic delay-channel-req))]
-;      (is (= waited 2)))))
-;
-;(deftest delay-channel-spell-go
-;  (cljs.test/async
-;   done
-;   (go
-;     (let [[tag response] (<! (channel work-magic delay-channel-req))]
-;       (is (= tag :succeed))
-;       (is (= response {:waited 2})))
-;     (done))))
-;
-;(deftest delay-fail-spell
-;  (-> (invoke work-magic
-;              {:magic-word (:magic-word config)
-;               :spell :delay-fail
-;               :msecs 3})
-;      with-some-error
-;      with-promised-completion))
+(use-fixtures
+  :each
+  {:before (fn [] (println "starting...")
+             (aset (.-env nodejs/process) "ORG_NAME" "COPA"))
+   :after  (fn [] (println "tear-down...")
+             (aset (.-env nodejs/process) "ORG_NAME" nil))})
+
+(deftest test-get-env
+  (testing "get retrieval of node's process.env.var"
+    (is (= (get-env "ORG_NAME") "COPA"))))
