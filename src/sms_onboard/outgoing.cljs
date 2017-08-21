8e4e0c8e3127dc078007c0ab2945e15edc171e1e
eemshi <oreneemshi@gmail.com>
2017-08-20 21:31:40 -0600

    rm unnecessary language from outgoing responses

--- src/sms_onboard/outgoing.cljs
+++ src/sms_onboard/outgoing.cljs
@@ -5,9 +5,8 @@
 
 (def messages
   {:retry  "Not a valid response. Please try again. \n\nRespuesta incorrecta, por favor vuelva a intentar."
-   :step-0 (format "Welcome to %s's text alerts and news! To subscribe to receive important updates reply \"yes.\" Do not respond if you do not want to be added at this time.\n\nBienvenid@ a los alertos de texto de %s! Para inscribirse a recibir información importante, responda \"yes.\" No necesita responder si no quiere recibir mensajes por el momento." ORG_NAME ORG_NAME)
-   :step-1 (format "Thank you for signing up to receive %s’s text messages! To provide you the correct information, please provide your preferred language. Reply “1” for English or “2” for Spanish. \n\n¡Gracias por inscribirse a los mensajes de %s! Para darle la información correcta, por favor designe su idioma preferido. Responda “1” para inglés o “2” para español." ORG_NAME ORG_NAME)
-   :step-2 {:spanish "¡Gracias! Su idioma preferido es español. Y para comunicarnos mejor, responda con su nombre completo."
-            :english "Thank you! Your preferred language is English. To best communicate with you, please respond with your full name."}
+   :step-1 "Please reply \"1\" for English or \"2\" for Spanish. \n\nPor favor responda \"1\" para inglés o \"2\" para español."
+   :step-2 {:spanish "¡Gracias! Y para comunicarnos mejor, responda con su nombre completo."
+            :english "Thank you! To best communicate with you, please respond with your full name."}
    :step-3 {:spanish (format "¡Gracias! ¡Este pendiente de la información de %s!" ORG_NAME)
             :english (format "Thank you! Stay tuned for updates from %s!" ORG_NAME)}})
